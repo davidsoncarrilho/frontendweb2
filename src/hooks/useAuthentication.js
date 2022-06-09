7dcfe3fb53d034964c2b4ce517bcc570abfee3e2
Davidson Martins <davidson.martins@ciss.com.br>
2022-06-09 15:54:15 -0300

    Registrando usuario no sistema/firebase

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -9,7 +11,7 @@ import {
 import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
-  const [error, serError] = useState(null);
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
   //cleanup
@@ -18,8 +20,55 @@ export const useAuthentication = () => {
   const [cancelled, setCancelled] = useState(false);
   const auth = getAuth();
 
-  function checkIfIsCancelled(){
-    if(cancelled){
+  function checkIfIsCancelled() {
+    if (cancelled) {
       return;
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setLoading(true);
+    setError(null);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+
+      await updateProfile(user, {
+        displayName: data.displayName,
+      });
+
+      setLoading(false);
+      return user;
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      let systemErrorMessage;
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres";
+      } else if (error.messa.includes("email-already")) {
+        systemErrorMessage = "E-mail já cadastrado";
+      } else {
+        systemErrorMessage = "Ocorreu um erro,por favor tente mais tarde.";
+      }
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => setCancelled(true);
+  }, []);
+
+  return {
+    auth,
+    createUser,
+    error,
+    loading,
+  };
+};
