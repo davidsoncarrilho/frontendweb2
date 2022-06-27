a5368d5522aea54d69d8bafbe74263ea2c549bbe
Alex Silva <alex.silva@avioconsulting.com>
2022-06-27 10:53:32 -0300

    home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,36 +1,9 @@
-// CSS
 import styles from "./Home.module.css";
 
-// hooks
-import { useFetchDocuments } from "../../hooks/useFetchDocuments";
-import { useNavigate, Link } from "react-router-dom";
-
-// react
-import { useState } from "react";
-import Creche from "../../components/Creche";
-
-// components
-
 const Home = () => {
-  const { documents: creches, loading } = useFetchDocuments("creches");
-
-  const navigate = useNavigate();
-
-  const [query, setQuery] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    if (query) {
-      return navigate(`/search?q=${query}`);
-    }
-  };
-
-  console.log(loading);
-
   return (
     <div className={styles.home}>
-      <h2>Home</h2>
+      <h2>O SEU SISTEMA PARA ADMINISTRAR CRECHES</h2>
     </div>
   );
 };
