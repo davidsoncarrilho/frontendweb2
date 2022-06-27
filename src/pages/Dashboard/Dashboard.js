7da927b81d2749c90473086ff2b04049dbcc0258
Alex Silva <alex.silva@avioconsulting.com>
2022-06-27 10:53:23 -0300

    dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,41 +1,45 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Creche from "../../components/Creche";
+import { API } from "../../service/service";
 import styles from "./Dashboard.module.css";
 
 const Dashboard = () => {
+  const [creches, setCreches] = useState([]);
+
+  const navigate = useNavigate();
+
+  const getCreches = async () => {
+    const {
+      data: { creches },
+    } = await API.get("/creches");
+
+    setCreches(creches);
+  };
+
+  const handleCrecheClick = (creche) => {
+    navigate(`/dashboard/creche/${creche.id}`);
+  };
+
+  useEffect(() => {
+    getCreches();
+  }, []);
   return (
     <div className={styles.home}>
-      <h2>Lista de colégios</h2>
-      <div className="creche-list">
-        {/* {loading && <p>Carregando...</p>} */}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>
-        {/* {creches && creches.length === 0 && (
-        <div className={styles.noposts}>
-          <p>Não foram encontrados creches</p>
-          <Link to="/creche/create" className="btn">
-            Cadastrar Creche
-          </Link>
-        </div>
-      )} */}
-        {/* {creches &&
-        creches.map((creche) => <Creche key={creche.id} post={creche} />)} */}
-      </div>
+      <h2>Lista de creches</h2>
+      {creches.map((creche) => (
+        <button
+          onClick={() => handleCrecheClick(creche)}
+          key={creche.id}
+          className="creche-button"
+        >
+          <div className="creche-list">
+            <div className="creche-card">
+              <Creche creche={creche} />
+            </div>{" "}
+          </div>
+        </button>
+      ))}
     </div>
   );
 };
