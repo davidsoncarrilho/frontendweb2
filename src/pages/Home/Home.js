4dd51aaae01459ef068dc5e5fedd0fded003d306
Davidson Martins <davidson.martins@ciss.com.br>
2022-06-26 22:34:36 -0300

    Ajustes home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -30,38 +30,7 @@ const Home = () => {
 
   return (
     <div className={styles.home}>
-      <h2>Lista de colégios</h2>
-      <div className="creche-list">
-        {loading && <p>Carregando...</p>}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>{" "}
-        <div className="creche-card">
-          <Creche />
-        </div>
-        {/* {creches && creches.length === 0 && (
-          <div className={styles.noposts}>
-            <p>Não foram encontrados creches</p>
-            <Link to="/creche/create" className="btn">
-              Cadastrar Creche
-            </Link>
-          </div>
-        )} */}
-        {/* {creches &&
-          creches.map((creche) => <Creche key={creche.id} post={creche} />)} */}
-      </div>
+      <h2>Home</h2>
     </div>
   );
 };
